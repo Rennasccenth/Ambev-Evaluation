646b35c8038286c4b92c836e55054f3b94fdb651
Felipe Nunes <nullnes.sh@gmail.com>
2025-01-28 16:22:29 -0400

    chore: fix typo error

--- src/Ambev.DeveloperEvaluation.Common/HealthChecks/HealthChecksExtension.cs
+++ src/Ambev.DeveloperEvaluation.Common/HealthChecks/HealthChecksExtension.cs
@@ -74,13 +74,13 @@ public static class HealthChecksExtension
     /// </example>
     public static void UseBasicHealthChecks(this WebApplication app)
     {
-        var livenessOptions = WriteHealtCheckRespose(app, "liveness");
+        var livenessOptions = WriteHealthCheckResponse(app, "liveness");
         app.UseHealthChecks("/health/live", livenessOptions);
 
-        var readinessOptions = WriteHealtCheckRespose(app, "readiness");
+        var readinessOptions = WriteHealthCheckResponse(app, "readiness");
         app.UseHealthChecks("/health/ready", readinessOptions);
 
-        var healthOptions = WriteHealtCheckRespose(app, string.Empty);
+        var healthOptions = WriteHealthCheckResponse(app, string.Empty);
         app.UseHealthChecks("/health", healthOptions);
 
         var logger = app.Services.GetRequiredService<ILogger<HealthCheckService>>();
@@ -97,9 +97,9 @@ public static class HealthChecksExtension
     /// This method configures the following options:
     /// - Filters health checks based on the provided tag.
     /// - Sets specific HTTP status codes for different health states.
-    /// - Configures a custom response writer that generates a JSON with detailed health status information.
+    /// - Configure a custom response writer that generates a JSON with detailed health status information.
     /// </remarks>
-    private static HealthCheckOptions WriteHealtCheckRespose(this WebApplication app, string tag)
+    private static HealthCheckOptions WriteHealthCheckResponse(this WebApplication app, string tag)
     {
         var options = new HealthCheckOptions
         {
