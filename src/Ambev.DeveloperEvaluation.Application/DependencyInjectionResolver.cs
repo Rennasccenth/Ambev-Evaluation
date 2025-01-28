1339dd6b2ce9c0b5c5fe2beda91a76fcdb4e8f35
Felipe Nunes <nullnes.sh@gmail.com>
2025-01-28 14:16:24 -0400

    Initial commit

---
+++ src/Ambev.DeveloperEvaluation.Application/DependencyInjectionResolver.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ambev.DeveloperEvaluation.Application;
+
+public static class DependencyInjectionResolver
+{
+    public static IServiceCollection InstallApplicationLayer(this IServiceCollection serviceCollection)
+    {
+        
+
+        return serviceCollection;
+    }
+}
