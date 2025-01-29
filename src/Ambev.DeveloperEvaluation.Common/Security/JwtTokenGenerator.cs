e861299a04d5cc9d3b293ca23b41b7849a91a81d
Felipe Nunes <nullnes.sh@gmail.com>
2025-01-28 21:35:18 -0400

    feat(wip/jwt): using options pattern to hydrate JwtTokenGenerator

--- src/Ambev.DeveloperEvaluation.Common/Security/JwtTokenGenerator.cs
+++ src/Ambev.DeveloperEvaluation.Common/Security/JwtTokenGenerator.cs
@@ -1,25 +1,28 @@
-using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 
 namespace Ambev.DeveloperEvaluation.Common.Security;
 
 /// <summary>
 /// Implementation of JWT (JSON Web Token) generator.
 /// </summary>
-public class JwtTokenGenerator : IJwtTokenGenerator
+public sealed class JwtTokenGenerator : IJwtTokenGenerator
 {
-    private readonly IConfiguration _configuration;
+    private readonly IOptions<JwtBearerOptions> _bearerTest;
+    private readonly JwtSettings _jwtSettings;
 
     /// <summary>
     /// Initializes a new instance of the JWT token generator.
     /// </summary>
-    /// <param name="configuration">Application configuration containing the necessary keys for token generation.</param>
-    public JwtTokenGenerator(IConfiguration configuration)
+    /// <param name="jwtOptions">Configured <see cref="JwtSettings"/> via <see cref="IOptions{TOptions}"/></param>
+    public JwtTokenGenerator(IOptions<JwtSettings> jwtOptions, IOptions<JwtBearerOptions> bearerTest)
     {
-        _configuration = configuration;
+        _bearerTest = bearerTest;
+        _jwtSettings = jwtOptions.Value;
     }
 
     /// <summary>
@@ -35,12 +38,12 @@ public class JwtTokenGenerator : IJwtTokenGenerator
     /// 
     /// The token is valid for 8 hours from the moment of generation.
     /// </remarks>
-    /// <exception cref="ArgumentNullException">Thrown when user or secret key is not provided.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when a user or secret key is not provided.</exception>
     public string GenerateToken(IUser user)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes(_configuration["Jwt:SecretKey"]);
-
+        var key = Encoding.ASCII.GetBytes(_jwtSettings.SecretKey);
+        var value = _bearerTest.Value;
         var claims = new[]
         {
            new Claim(ClaimTypes.NameIdentifier, user.Id),
