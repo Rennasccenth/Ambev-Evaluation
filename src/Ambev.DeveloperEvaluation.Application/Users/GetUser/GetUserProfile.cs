b08b9affb1167e4d0041a2fb1d2efc5a9dc43e7e
Felipe Nunes <nullnes.sh@gmail.com>
2025-01-29 00:52:58 -0400

    fix: adds a missing GetUserProfile map

--- src/Ambev.DeveloperEvaluation.Application/Users/GetUser/GetUserProfile.cs
+++ src/Ambev.DeveloperEvaluation.Application/Users/GetUser/GetUserProfile.cs
@@ -13,6 +13,12 @@ public class GetUserProfile : Profile
     /// </summary>
     public GetUserProfile()
     {
-        CreateMap<User, GetUserResult>();
+        CreateMap<User, GetUserResult>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Username))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+            .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone))
+            .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
     }
 }
--- src/Ambev.DeveloperEvaluation.WebApi/Features/Users/GetUser/GetUserProfile.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Users/GetUser/GetUserProfile.cs
@@ -1,3 +1,4 @@
+using Ambev.DeveloperEvaluation.Application.Users.GetUser;
 using AutoMapper;
 
 namespace Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
@@ -12,7 +13,16 @@ public class GetUserProfile : Profile
     /// </summary>
     public GetUserProfile()
     {
-        CreateMap<Guid, Application.Users.GetUser.GetUserCommand>()
-            .ConstructUsing(id => new Application.Users.GetUser.GetUserCommand(id));
+        CreateMap<Guid, GetUserCommand>()
+            .ConstructUsing(id => new GetUserCommand(id));
+        
+        CreateMap<GetUserResult, GetUserResponse>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+            .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone))
+            .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
+
     }
 }
