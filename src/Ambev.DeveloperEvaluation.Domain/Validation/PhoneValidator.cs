78d3566068d019cacb1baf5a0439455c842fea25
Felipe Nunes <nullnes.sh@gmail.com>
2025-01-31 03:40:31 -0400

    fix: phone regex wasn't accepting 0 at start

--- src/Ambev.DeveloperEvaluation.Domain/Validation/PhoneValidator.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Validation/PhoneValidator.cs
@@ -8,6 +8,7 @@ public class PhoneValidator : AbstractValidator<string>
     {
         RuleFor(phone => phone)
             .NotEmpty().WithMessage("The phone cannot be empty.")
-            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("The phone format is not valid.");
+            .Matches(@"^\+?[0-9]\d{1,14}$")
+            .WithMessage("The phone format is not valid.");
     }
 }
