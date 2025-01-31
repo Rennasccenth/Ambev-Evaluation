4aa4aed8d8ba179e0d908453e1a7c98035dfb5ee
Felipe Nunes <nullnes.sh@gmail.com>
2025-01-30 20:58:28 -0400

    feat(user): adds fluent builder for User entity + Add missing fields

--- src/Ambev.DeveloperEvaluation.Domain/Entities/User.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/User.cs
@@ -1,146 +1,138 @@
 using Ambev.DeveloperEvaluation.Common.Security;
-using Ambev.DeveloperEvaluation.Common.Validation;
 using Ambev.DeveloperEvaluation.Domain.Common;
 using Ambev.DeveloperEvaluation.Domain.Enums;
-using Ambev.DeveloperEvaluation.Domain.Validation;
+using Ambev.DeveloperEvaluation.Domain.ValueObjects;
+using FluentValidation;
 using FluentValidation.Results;
 
 namespace Ambev.DeveloperEvaluation.Domain.Entities;
 
-
 /// <summary>
-/// Represents a user in the system with authentication and profile information.
+/// Represents a user in the system.
 /// This entity follows domain-driven design principles and includes business rules validation.
 /// </summary>
-public class User : BaseEntity, IUser
+public partial class User : BaseEntity
 {
-    /// <summary>
-    /// Gets the user's full name.
-    /// Must not be null or empty and should contain both first and last names.
-    /// </summary>
-    public string Username { get; set; } = string.Empty;
+    // The only way to build User entities is going by Builder, unless you use some internal scan
+    internal User() { }
 
-    /// <summary>
-    /// Gets the user's email address.
-    /// Must be a valid email format and is used as a unique identifier for authentication.
-    /// </summary>
-    public string Email { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets the user's phone number.
-    /// Must be a valid phone number format following the pattern (XX) XXXXX-XXXX.
-    /// </summary>
-    public string Phone { get; set; } = string.Empty ;
-
-    /// <summary>
-    /// Gets the hashed password for authentication.
-    /// Password must meet security requirements: minimum 8 characters, at least one uppercase letter,
-    /// one lowercase letter, one number, and one special character.
-    /// </summary>
-    public string Password { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets the user's role in the system.
-    /// Determines the user's permissions and access levels.
-    /// </summary>
-    public UserRole Role { get;     set; }
-
-    /// <summary>
-    /// Gets the user's current status.
-    /// Indicates whether the user is active, inactive, or blocked in the system.
-    /// </summary>
+    public string Username { get; internal set; } = null!;
+    public string Email { get; set; } = null!;
+    public Address Address { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+    public string Firstname { get; set; } = null!;
+    public string Lastname { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public UserRole Role { get; set; }
     public UserStatus Status { get; set; }
-
-    /// <summary>
-    /// Gets the date and time when the user was created.
-    /// </summary>
     public DateTime CreatedAt { get; set; }
-
-    /// <summary>
-    /// Gets the date and time of the last update to the user's information.
-    /// </summary>
     public DateTime? UpdatedAt { get; set; }
 
-    /// <summary>
-    /// Gets the unique identifier of the user.
-    /// </summary>
-    /// <returns>The user's ID as a string.</returns>
-    string IUser.Id => Id.ToString();
+    public static UserBuilder GetBuilder(
+        IPasswordHasher passwordHasher,
+        TimeProvider timeProvider,
+        IValidator<User> userValidator)
+        => new (passwordHasher, timeProvider, userValidator);
 
     /// <summary>
-    /// Gets the username.
+    /// Fluent builder pattern for User, intended to build User entities while keep them valid.
     /// </summary>
-    /// <returns>The username.</returns>
-    string IUser.Username => Username;
-
-    /// <summary>
-    /// Gets the user's role in the system.
-    /// </summary>
-    /// <returns>The user's role as a string.</returns>
-    string IUser.Role => Role.ToString();
-
-    /// <summary>
-    /// Initializes a new instance of the User class.
-    /// </summary>
-    public User()
+    public sealed class UserBuilder
     {
-        CreatedAt = DateTime.UtcNow;
-    }
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly TimeProvider _timeProvider;
+        private readonly IValidator<User> _userValidator;
+        private readonly User _buildingUser;
 
-    /// <summary>
-    /// Performs validation of the user entity using the UserValidator rules.
-    /// </summary>
-    /// <returns>
-    /// A <see cref="ValidationResultDetail"/> containing:
-    /// - IsValid: Indicates whether all validation rules passed
-    /// - Errors: Collection of validation errors if any rules failed
-    /// </returns>
-    /// <remarks>
-    /// <listheader>The validation includes checking:</listheader>
-    /// <list type="bullet">Username format and length</list>
-    /// <list type="bullet">Email format</list>
-    /// <list type="bullet">Phone number format</list>
-    /// <list type="bullet">Password complexity requirements</list>
-    /// <list type="bullet">Role validity</list>
-    /// 
-    /// </remarks>
-    public ValidationResultDetail Validate()
-    {
-        ValidationResult? result = new UserValidator().Validate(this);
-        return new ValidationResultDetail
+        internal UserBuilder(
+            IPasswordHasher passwordHasher,
+            TimeProvider timeProvider,
+            IValidator<User> userValidator)
         {
-            IsValid = result.IsValid,
-            Errors = result.Errors.Select(o => (ValidationErrorDetail)o)
-        };
+            _passwordHasher = passwordHasher;
+            _timeProvider = timeProvider;
+            _userValidator = userValidator;
+            _buildingUser = new User();
+        }
+        public UserBuilder WithEmail(string email)
+        {
+            _buildingUser.Email = email;
+            return this;
+        }
+        public UserBuilder WithUsername(string username)
+        {
+            _buildingUser.Username = username;
+            return this;
+        }
+        public UserBuilder WithPassword(string password)
+        {
+            _buildingUser.Password = _passwordHasher.HashPassword(password);
+            return this;
+        }
+        public UserBuilder WithFirstname(string firstname)
+        {
+            _buildingUser.Firstname = firstname;
+            return this;
+        }
+        public UserBuilder WithLastname(string lastname)
+        {
+            _buildingUser.Lastname = lastname;
+            return this;
+        }
+        public UserBuilder WithAddress(Address address)
+        {
+            _buildingUser.Address = address;
+            return this;
+        }
+        public UserBuilder WithStatus(UserStatus status)
+        {
+            _buildingUser.Status = status;
+            return this;
+        }
+        public UserBuilder WithPhone(string phone)
+        {
+            _buildingUser.Phone = phone;
+            return this;
+        }
+        public UserBuilder WithRole(UserRole role)
+        {
+            _buildingUser.Role = role;
+            return this;
+        }
+
+        public OneOf<User, ValidationResult> Build()
+        {
+            _buildingUser.CreatedAt = _timeProvider.GetUtcNow().DateTime;
+
+            ValidationResult? validationResult = _userValidator.Validate(_buildingUser);
+            if (validationResult.IsValid) return _buildingUser;
+
+            return validationResult;
+        }
     }
 
-    /// <summary>
-    /// Activates the user account.
-    /// Changes the user's status to Active.
-    /// </summary>
-    public void Activate()
+    public void Activate(TimeProvider timeProvider)
     {
         Status = UserStatus.Active;
-        UpdatedAt = DateTime.UtcNow;
+        UpdatedAt = timeProvider.GetUtcNow().DateTime;
     }
 
-    /// <summary>
-    /// Deactivates the user account.
-    /// Changes the user's status to Inactive.
-    /// </summary>
-    public void Deactivate()
+    public void Deactivate(TimeProvider timeProvider)
     {
         Status = UserStatus.Inactive;
-        UpdatedAt = DateTime.UtcNow;
+        UpdatedAt = timeProvider.GetUtcNow().DateTime;
     }
 
-    /// <summary>
-    /// Blocks the user account.
-    /// Changes the user's status to Blocked.
-    /// </summary>
-    public void Suspend()
+    public void Suspend(TimeProvider timeProvider)
     {
         Status = UserStatus.Suspended;
-        UpdatedAt = DateTime.UtcNow;
+        UpdatedAt = timeProvider.GetUtcNow().DateTime;
     }
+}
+
+public partial class User : IUser
+{
+    string IUser.Id => Id.ToString();
+    string IUser.Username => Username;
+    string IUser.Role => Role.ToString();
 }
