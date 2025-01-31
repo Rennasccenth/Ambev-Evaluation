60990f7b8097491fce188a7f90d61e27fad3b8c0
Felipe Nunes <nullnes.sh@gmail.com>
2025-01-30 21:03:48 -0400

    feat(database): adds user table missing fields

---
+++ src/Ambev.DeveloperEvaluation.ORM/Configurations/PostgreSQLTypeConstants.cs
@@ -0,0 +1,6 @@
+namespace Ambev.DeveloperEvaluation.ORM.Configurations;
+
+internal static class PostgreSqlTypeConstants
+{
+    internal const string DateTimeType = "TIMESTAMP WITHOUT TIME ZONE";
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Configurations/UserConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id)
+            .HasColumnType("uuid")
+            .HasDefaultValueSql("gen_random_uuid()");
+        builder.Property(u => u.Email).IsRequired().HasMaxLength(100);
+        builder.Property(u => u.Firstname).IsRequired().HasMaxLength(50);
+        builder.Property(u => u.Lastname).IsRequired().HasMaxLength(50);
+        builder.Property(u => u.Username).IsRequired().HasMaxLength(50);
+        builder.Property(u => u.Password).IsRequired().HasMaxLength(100);
+        builder.ComplexProperty(u => u.Address, propertyBuilder =>
+        {
+            propertyBuilder.Property(addr => addr.Street).IsRequired().HasMaxLength(50);
+            propertyBuilder.Property(addr => addr.City).IsRequired().HasMaxLength(50);
+            propertyBuilder.Property(addr => addr.Number).IsRequired();
+            propertyBuilder.Property(addr => addr.ZipCode).IsRequired().HasMaxLength(50);
+            propertyBuilder.Property(addr => addr.Latitude).IsRequired().HasMaxLength(15);
+            propertyBuilder.Property(addr => addr.Longitude).IsRequired().HasMaxLength(15);
+        });
+        builder.Property(u => u.Phone).HasMaxLength(20);
+
+        builder.Property(u => u.Status)
+            .HasConversion<string>()
+            .HasMaxLength(20);
+
+        builder.Property(u => u.Role)
+            .HasConversion<string>()
+            .HasMaxLength(20);
+
+        builder.Property(u => u.CreatedAt)
+            .IsRequired()
+            .HasColumnType(PostgreSqlTypeConstants.DateTimeType);
+
+        builder.Property(u => u.CreatedAt)
+            .HasColumnType(PostgreSqlTypeConstants.DateTimeType);
+    }
+}
