ac06e35f1b1d2247db5f6f369ce9c4d1be9a687c
Felipe Nunes <nullnes.sh@gmail.com>
2025-02-03 18:55:30 -0400

    build: removing build warnings

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Users/Queries/GetUser/GetUserResponse.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Users/Queries/GetUser/GetUserResponse.cs
@@ -2,34 +2,34 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Users.Queries.GetUser;
 
 public sealed class GetUserResponse
 {
-    public required Guid Id { get; init; }
-    public required string Email { get; init; }
-    public required string Username { get; init; }
-    public required string Password { get; init; }
-    public required NameDto Name { get; init; }
-    public required AddressDto Address { get; init; }
-    public required string Phone { get; init; }
-    public required string Status { get; init; }
-    public required string Role { get; init; }
+    public Guid Id { get; init; }
+    public string Email { get; init; } = null!;
+    public string Username { get; init; } = null!;
+    public string Password { get; init; } = null!;
+    public NameDto Name { get; init; } = null!;
+    public AddressDto Address { get; init; } = null!;
+    public string Phone { get; init; } = null!;
+    public string Status { get; init; } = null!;
+    public string Role { get; init; } = null!;
 }
 
 public sealed record AddressDto
 {
-    public string City { get; init; }
-    public string Street { get; init; }
+    public string City { get; init; } = null!;
+    public string Street { get; init; } = null!;
     public int Number { get; init; }
-    public string Zipcode { get; init; }
-    public GeolocationDto Geolocation { get; init; }
+    public string Zipcode { get; init; } = null!;
+    public GeolocationDto Geolocation { get; init; } = null!;
 }
 
 public sealed record GeolocationDto
 {
-    public string Lat { get; init; }
-    public string Long { get; init; }
+    public string Lat { get; init; } = null!;
+    public string Long { get; init; } = null!;
 }
 
 public class NameDto
 {
-    public string Firstname { get; init; }
-    public string Lastname { get; init; }
+    public string Firstname { get; init; } = null!;
+    public string Lastname { get; init; } = null!;
 }
