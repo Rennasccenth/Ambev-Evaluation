154b65411d6c8d3248ac3c967bddaf0b95a227fb
Felipe Nunes <nullnes.sh@gmail.com>
2025-02-02 20:50:31 -0400

    chore: remove empty line

--- src/Ambev.DeveloperEvaluation.Domain/Validation/UserValidator.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Validation/UserValidator.cs
@@ -22,6 +22,5 @@ public class UserValidator : AbstractValidator<User>
             .NotEmpty()
             .MinimumLength(3).WithMessage("Username must be at least 3 characters long.")
             .MaximumLength(50).WithMessage("Username cannot be longer than 50 characters.");
-        
     }
 }
