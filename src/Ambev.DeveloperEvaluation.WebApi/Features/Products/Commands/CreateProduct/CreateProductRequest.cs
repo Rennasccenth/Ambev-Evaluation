68da22226657a09c283fb0f8e545030e8a475a83
Felipe Nunes <nullnes.sh@gmail.com>
2025-02-03 18:33:50 -0400

    chore: remove unused annotations

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Products/Commands/CreateProduct/CreateProductRequest.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Products/Commands/CreateProduct/CreateProductRequest.cs
@@ -1,26 +1,13 @@
-using System.ComponentModel.DataAnnotations;
 using Ambev.DeveloperEvaluation.Domain.ValueObjects;
 
 namespace Ambev.DeveloperEvaluation.WebApi.Features.Products.Commands.CreateProduct;
 
 public sealed class CreateProductRequest
 {
-    [Required]
-    public string Title { get; init; }
-
-    [Required]
-    [Range(0.01, double.MaxValue)]
+    public string Title { get; init; } = null!;
     public decimal Price { get; init; }
-
-    [Required]
-    public string Description { get; init; }
-
-    [Required]
-    public string Category { get; init; }
-
-    [Required]
-    public string Image { get; init; }
-
-    [Required]
-    public Rating Rating { get; init; }
+    public string Description { get; init; } = null!;
+    public string Category { get; init; } = null!;
+    public string Image { get; init; } = null!;
+    public Rating Rating { get; init; } = null!;
 }
