50271beb26ecf085e0ff781a3ca7bad988dd8fd6
Felipe Nunes <nullnes.sh@gmail.com>
2025-02-04 16:39:15 -0400

    feat: adds ISaleService domain service

---
+++ src/Ambev.DeveloperEvaluation.Domain/Services/Abstractions/ISalesService.cs
@@ -0,0 +1,15 @@
+using Ambev.DeveloperEvaluation.Domain.Aggregates.Carts;
+using Ambev.DeveloperEvaluation.Domain.Aggregates.Sales;
+
+namespace Ambev.DeveloperEvaluation.Domain.Services.Abstractions;
+
+public interface ISalesService
+{
+    Task<Sale> CreateSaleAsync(Cart cart, IProductPriceResolver productPriceResolver, CancellationToken ct);
+    // Task<Sale> UpdateSaleAsync(Cart cart, IProductPriceResolver productPriceResolver, CancellationToken ct);
+    Task<Sale> CancelSaleAsync(Guid saleId, CancellationToken ct);
+    Task<Sale> ConcludeSaleAsync(Guid saleId, CancellationToken ct);
+}
+
+
+
