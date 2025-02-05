b22f65694056b9a0314c855c099d8dc9cb89938b
Felipe Nunes <nullnes.sh@gmail.com>
2025-02-05 01:40:45 -0400

    feat: update ProductInventory.cs

--- src/Ambev.DeveloperEvaluation.Domain/Aggregates/Inventories/ProductInventory.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Aggregates/Inventories/ProductInventory.cs
@@ -4,11 +4,13 @@ namespace Ambev.DeveloperEvaluation.Domain.Aggregates.Inventories;
 
 public sealed class ProductInventory : BaseEntity
 {
+    public Guid ProductId { get; set; }
     public int Quantity { get; private set; }
 
     public ProductInventory(Guid productId, int initialQuantity)
     {
-        Id = productId;
+        Id = Guid.NewGuid();
+        ProductId = productId;
         Quantity = initialQuantity;
     }
 }
