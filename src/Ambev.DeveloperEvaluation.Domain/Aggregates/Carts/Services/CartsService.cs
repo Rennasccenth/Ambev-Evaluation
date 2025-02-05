01108a02f996f3a3ee1d70529cd3c027f52a57af
Felipe Nunes <nullnes.sh@gmail.com>
2025-02-05 01:42:14 -0400

    security: add verifications about cart lifecycle management

--- src/Ambev.DeveloperEvaluation.Domain/Aggregates/Carts/Services/CartsService.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Aggregates/Carts/Services/CartsService.cs
@@ -1,3 +1,5 @@
+using System.Security;
+using Ambev.DeveloperEvaluation.Common.Security;
 using Ambev.DeveloperEvaluation.Domain.Aggregates.Carts.Exceptions;
 using Ambev.DeveloperEvaluation.Domain.Aggregates.Carts.Repositories;
 using Ambev.DeveloperEvaluation.Domain.Aggregates.Products.Repositories;
@@ -9,26 +11,30 @@ public sealed class CartsService : ICartsService
     private readonly TimeProvider _timeProvider;
     private readonly ICartRepository _cartRepository;
     private readonly IProductRegistryRepository _productRegistryRepository;
+    private readonly IUserContext _userContext;
 
     public CartsService(TimeProvider timeProvider,
         ICartRepository cartRepository,
-        IProductRegistryRepository productRegistryRepository)
+        IProductRegistryRepository productRegistryRepository,
+        IUserContext userContext)
     {
         _timeProvider = timeProvider;
         _cartRepository = cartRepository;
         _productRegistryRepository = productRegistryRepository;
+        _userContext = userContext;
     }
 
     public async Task<Cart> UpsertCartProductsAsync(Guid userId, Dictionary<Guid, uint> productQuantitiesDictionary, CancellationToken ct = default)
     {
+        EnsureCartOwnership(userId);
+
         var allProductsExists = await _productRegistryRepository.ExistsAllAsync(productQuantitiesDictionary.Keys, ct);
         if (!allProductsExists)
         {
             throw new CartValidationException("Not every product in the list exists.");
         }
 
-        Cart userCart = await _cartRepository.FindByUserIdAsync(userId, ct) 
-                         ?? new Cart(userId, _timeProvider.GetUtcNow().DateTime);
+        Cart userCart = await GetOrCreateUserCart(userId, ct);
 
         foreach ((Guid productId, var productQuantity) in productQuantitiesDictionary)
         {
@@ -39,8 +45,23 @@ public sealed class CartsService : ICartsService
         return userCart;
     }
 
+    public async Task<Cart> GetOrCreateUserCart(Guid userId, CancellationToken ct)
+    {
+        EnsureCartOwnership(userId);
+
+        Cart? foundCart = await _cartRepository.FindByUserIdAsync(userId, ct);
+
+        if (foundCart is not null) return foundCart;
+
+        foundCart = await _cartRepository.UpsertAsync(new Cart(userId, _timeProvider.GetUtcNow().DateTime), ct);
+
+        return foundCart;
+    }
+
     public async Task<Cart> RemoveCartProductsAsync(Guid userId, Dictionary<Guid, uint> productQuantitiesDictionary, CancellationToken ct)
     {
+        EnsureCartOwnership(userId);
+
         Cart? userCart = await _cartRepository.FindByUserIdAsync(userId, ct);
         if (userCart is null)
         {
@@ -51,5 +72,25 @@ public sealed class CartsService : ICartsService
             userCart.RemoveProduct(productId, (int)productQuantity);
         }
         return userCart;
+    }
+
+    public Task RemoveCartAsync(Guid userId, CancellationToken ct)
+    {
+        EnsureCartOwnership(userId);
+
+        return _cartRepository.DeleteAsync(userId, ct);
+    }
+
+
+    /// <summary>
+    /// Verify if the current logged user has the ownership of the cart.
+    /// </summary>
+    /// <exception cref="SecurityException"></exception>
+    private void EnsureCartOwnership(Guid userId)
+    {
+        if (_userContext.IsAuthenticated && _userContext.UserId != userId)
+        {
+            throw new SecurityException($"Invalid cart access for userId {userId}. The following UserId {_userContext.UserId} tried to access it.");
+        }
     }
 }
--- src/Ambev.DeveloperEvaluation.Domain/Aggregates/Carts/Services/ICartsService.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Aggregates/Carts/Services/ICartsService.cs
@@ -6,7 +6,10 @@ public interface ICartsService
     /// Adds products into the user cart. If the user doens't own one, it was created. If theres overlapping products,
     /// their quantities will be updated
     /// </summary>
+    Task<Cart> GetOrCreateUserCart(Guid userId, CancellationToken ct);
+    
     Task<Cart> UpsertCartProductsAsync(Guid userId, Dictionary<Guid, uint> productQuantitiesDictionary, CancellationToken ct);
 
     Task<Cart> RemoveCartProductsAsync(Guid userId, Dictionary<Guid, uint> productQuantitiesDictionary, CancellationToken ct);
+    Task RemoveCartAsync(Guid userId, CancellationToken ct);
 }
