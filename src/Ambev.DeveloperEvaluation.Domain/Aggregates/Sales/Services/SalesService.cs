57c424531b030c5e33ad877ad64051883d6b6949
Felipe Nunes <nullnes.sh@gmail.com>
2025-02-04 21:36:28 -0400

    feat: adds logging in sales domain service

--- src/Ambev.DeveloperEvaluation.Domain/Aggregates/Sales/Services/SalesService.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Aggregates/Sales/Services/SalesService.cs
@@ -5,6 +5,7 @@ using Ambev.DeveloperEvaluation.Domain.Aggregates.Sales.Exceptions;
 using Ambev.DeveloperEvaluation.Domain.Events.Abstractions;
 using Ambev.DeveloperEvaluation.Domain.Repositories.Sales;
 using Ambev.DeveloperEvaluation.Domain.Specifications;
+using Microsoft.Extensions.Logging;
 
 namespace Ambev.DeveloperEvaluation.Domain.Aggregates.Sales.Services;
 
@@ -14,17 +15,20 @@ public sealed class SalesService : ISalesService
     private readonly ISaleRepository _saleRepository;
     private readonly IDiscountStrategy _discountStrategy;
     private readonly IDomainEventDispatcher _domainEventDispatcher;
+    private readonly ILogger<SalesService> _logger;
 
     public SalesService(
         TimeProvider timeProvider,
         ISaleRepository saleRepository,
         IDiscountStrategy discountStrategy,
-        IDomainEventDispatcher domainEventDispatcher)
+        IDomainEventDispatcher domainEventDispatcher,
+        ILogger<SalesService> logger)
     {
         _timeProvider = timeProvider;
         _saleRepository = saleRepository;
         _discountStrategy = discountStrategy;
         _domainEventDispatcher = domainEventDispatcher;
+        _logger = logger;
     }
 
     public async Task<Sale> CreateSaleAsync(
@@ -34,14 +38,17 @@ public sealed class SalesService : ISalesService
         ISpecification<Cart> specification,
         CancellationToken ct = default)
     {
+        _logger.LogInformation("Creating sale for customer {CustomerId}", cart.CustomerId);
+        
         var productsUnitPrices = await productPriceResolver.ResolveProductsUnitPriceAsync(cart.Products.Select(p => p.ProductId), ct);
         List<SaleProduct> sellingProducts = [];
 
-        if (specification.IsSatisfiedBy(cart))
+        if (!specification.IsSatisfiedBy(cart))
         {
             throw new CartValidationException("Cart is invalid.");
         }
-        
+
+        // Convert all products in the cart to sale products.
         foreach ((Guid productId, var unitPrice) in productsUnitPrices)
         {
             int productCount = cart.CountProducts(p => p.ProductId == productId);
@@ -52,15 +59,25 @@ public sealed class SalesService : ISalesService
             sellingProducts.Add(new SaleProduct(productId, discountedPrice, productCount));
         }
 
-        var creatingSale = Sale.Create(cart.CustomerId, sellingProducts, branch, _timeProvider);
+        // Initialize products for the creating sale. 
+        var creatingSale = Sale.Create(cart.CustomerId, branch, _timeProvider);
+        foreach (SaleProduct saleProduct in sellingProducts)
+        {
+            creatingSale.AddProduct(saleProduct, _timeProvider);
+        }
+
         Sale createdSale = await _saleRepository.CreateAsync(creatingSale, ct);
 
+        // Dispatch any events
         await _domainEventDispatcher.DispatchAndClearEventsAsync(createdSale);
+
+        _logger.LogInformation("Sale created for customer {CustomerId}", cart.CustomerId);
         return createdSale;
     }
 
     public async Task<Sale?> CancelSaleAsync(Guid saleId, CancellationToken ct)
     {
+        _logger.LogInformation("Cancelling sale {SaleId}", saleId);
         var sale = await _saleRepository.FindByIdAsync(saleId, ct);
         if (sale is null)
         {
@@ -69,11 +86,15 @@ public sealed class SalesService : ISalesService
 
         var canceledSale = sale.Cancel(_timeProvider);
         await _domainEventDispatcher.DispatchAndClearEventsAsync(canceledSale);
+        
+        _logger.LogInformation("Sale {SaleId} cancelled", saleId);
         return canceledSale;
     }
 
     public async Task<Sale?> ConcludeSaleAsync(Guid saleId, CancellationToken ct)
     {
+        _logger.LogInformation("Concluding sale {SaleId}", saleId);
+        
         var sale = await _saleRepository.FindByIdAsync(saleId, ct);
         if (sale is null)
         {
@@ -82,6 +103,8 @@ public sealed class SalesService : ISalesService
         
         var concludedSale = sale.Cancel(_timeProvider);
         await _domainEventDispatcher.DispatchAndClearEventsAsync(concludedSale);
+        
+        _logger.LogInformation("Sale {SaleId} concluded", saleId);
         return concludedSale;
     }
 }
